09336e63240919294073a863b4eab4e61d3e6f0f
Jennifer Smith <jennifer@home>
2014-06-09 09:26:38 +1000

    The collection tests work with a fix to core.logic 118

--- test/kibit/test/collections.clj
+++ test/kibit/test/collections.clj
@@ -12,23 +12,13 @@
     '(vector a b) '(conj [] a b)
     '(vec coll) '(into [] coll)
     '(set coll) '(into #{} coll)
-;;    '(update-in coll [k] f) '(assoc coll k (f (k coll)))
-;;    '(update-in coll [k] f) '(assoc coll k (f (coll k)))
-;;    '(update-in coll [k] f) '(assoc coll k (f (get coll k)))
-;;    '(update-in coll [k] f a b c) '(assoc coll k (f (k coll) a b c))
-;;    '(update-in coll [k] f a b c) '(assoc coll k (f (coll k) a b c))
-;;    '(update-in coll [k] f a b c) '(assoc coll k (f (get coll k) a b c))
+    '(update-in coll [k] f) '(assoc coll k (f (k coll)))
+    '(update-in coll [k] f) '(assoc coll k (f (coll k)))
+    '(update-in coll [k] f) '(assoc coll k (f (get coll k)))
+    '(update-in coll [k] f a b c) '(assoc coll k (f (k coll) a b c))
+    '(update-in coll [k] f a b c) '(assoc coll k (f (coll k) a b c))
+    '(update-in coll [k] f a b c) '(assoc coll k (f (get coll k) a b c))
     '(assoc-in coll [k1 k2] v) '(update-in coll [k1 k2] assoc v)
     '(repeatedly 10 (constantly :foo)) '(take 10 (repeatedly (constantly :foo)))))
 
-(deftest collections-are-broken
-  (are
-       [expected-alt-form test-form]
-    (= expected-alt-form (:alt (kibit/check-expr test-form)))
-    '(update-in coll [?key] f) '(assoc coll k (f (k coll)))
-    '(update-in coll [?key] f) '(assoc coll k (f (coll k)))
-    '(update-in coll [?key] f) '(assoc coll k (f (get coll k)))
-    '(update-in coll [?key] f a b c) '(assoc coll k (f (k coll) a b c))
-    '(update-in coll [?key] f a b c) '(assoc coll k (f (coll k) a b c))
-    '(update-in coll [?key] f a b c) '(assoc coll k (f (get coll k) a b c)))
-  )
+
